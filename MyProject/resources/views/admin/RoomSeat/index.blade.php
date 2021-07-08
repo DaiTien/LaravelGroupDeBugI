2003e6b71f452d7441fc4d8dff5dd3512a50ed0f
quoccluongg <luongd872@gmail.com>
2021-07-09 00:05:02 +0700

    Luong

--- MyProject/resources/views/admin/RoomSeat/index.blade.php
+++ MyProject/resources/views/admin/RoomSeat/index.blade.php
@@ -2,56 +2,63 @@
 @section('titleForm')
     <div class="Page breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;<a href="/admin">{{__('Home')}}</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{__('TitleLinkRoomSeat')}}</li>
+            <li class="breadcrumb-item active" aria-current="page"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;<a
+                    href="/admin">{{ __('Home') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page">{{ __('TitleLinkRoomSeat') }}</li>
         </ol>
     </div>
 @endsection
 @section('content')
     <div class="card mb-4">
         <div class="m-2">
-            <h3 class="text-center">{{__('TitleListRoomSeat')}}</h3>
+            <h3 class="text-center">{{ __('TitleListRoomSeat') }}</h3>
         </div>
         <form method="POST">
             @csrf
             <div class="card-body">
-                @if(isset($room))
+                @if (isset($room))
                     <div class="form-group row">
                         <label class="col-sm-2">Phòng chiếu: </label>
                         <div class="col-sm-4">
                             <select name="room" class="form-control list_room">
-                                @foreach($room as $room)
-                                    <option value="{{$room->id}}">{{$room->name}}</option>
+                                @foreach ($room as $room)
+                                    <option value="{{ $room->id }}">{{ $room->name }}</option>
                                 @endforeach
                             </select>
                         </div>
                     </div>
                 @endif
                 <div class="table-responsive">
-                    @if(isset($data))
+                    @if (isset($data))
                         <h4>Sơ đồ phòng chiếu</h4>
                         <hr>
                         <table class="table table-borderless mx-auto w-auto">
                             <thead>
-                            <tr>
-                                <th class="text-center">Dãy ghế</th>
-                                <th></th>
-                            </tr>
+                                <tr>
+                                    <th class="text-center">Dãy ghế</th>
+                                    <th></th>
+                                </tr>
                             </thead>
                             <tbody class="data">
-                            @foreach($data['name_seat'] as $name_seat)
-                                <tr>
-                                    <th class="text-center ">
-                                        <a href="javascript:void(0)" id="{{$name_seat->id}}" class="row_seat active">{{$name_seat->name}}</a>
-                                    </th>
-                                    <td>
-                                        @foreach($data['seat'][$name_seat->name] as $seat)
-                                            <input type="checkbox" id="check_{{$seat->id}}" name="{{$name_seat->name.'_'.$seat->seat_number}}" value="{{$seat->id}}" style="display: none"/>
-                                            <a href="javascript:void(0)" onclick="changStatus({{$seat->id}})" id="{{$seat->id}}" name="{{$name_seat->name.'_'.$seat->seat_number}}" class="seat {{$seat->status ==0 ? "active":""}}">{{$seat->seat_number}}</a>
-                                        @endforeach
-                                    </td>
-                                </tr>
-                            @endforeach
+                                @foreach ($data['name_seat'] as $name_seat)
+                                    <tr>
+                                        <th class="text-center ">
+                                            <a href="javascript:void(0)" id="{{ $name_seat->id }}"
+                                                class="row_seat active">{{ $name_seat->name }}</a>
+                                        </th>
+                                        <td>
+                                            @foreach ($data['seat'][$name_seat->name] as $seat)
+                                                <input type="checkbox" id="check_{{ $seat->id }}"
+                                                    name="{{ $name_seat->name . '_' . $seat->seat_number }}"
+                                                    value="{{ $seat->id }}" style="display: none" />
+                                                <a href="javascript:void(0)" onclick="changStatus({{ $seat->id }})"
+                                                    id="{{ $seat->id }}"
+                                                    name="{{ $name_seat->name . '_' . $seat->seat_number }}"
+                                                    class="seat {{ $seat->status == 0 ? 'active' : '' }}">{{ $seat->seat_number }}</a>
+                                            @endforeach
+                                        </td>
+                                    </tr>
+                                @endforeach
 
                             </tbody>
                         </table>
@@ -61,17 +68,18 @@
                 </div>
             </div>
             <div hidden>
-                <input id="txtDanhSachChecked" name="ds_id" type="text"/>
-                <input id="txtCountChecked" name="count" type="text"/>
+                <input id="txtDanhSachChecked" name="ds_id" type="text" />
+                <input id="txtCountChecked" name="count" type="text" />
             </div>
             <div class="form-group text-center">
-                <a href="javascript:void(0)" onclick="disableSeat()" id="btnDisable" class="btn btn-primary" style="pointer-events: none">{{__('TextDisable')}}</a>
-                <a href="javascript:void(0)" onclick="enableSeat()" id="btnEnable" class="btn btn-primary" style="pointer-events: none">{{__('TextEnable')}}</a>
+                <a href="javascript:void(0)" onclick="disableSeat()" id="btnDisable" class="btn btn-light"
+                    style="pointer-events: none">{{ __('TextDisable') }}</a>
+                <a href="javascript:void(0)" onclick="enableSeat()" id="btnEnable" class="btn btn-light"
+                    style="pointer-events: none">{{ __('TextEnable') }}</a>
             </div>
         </form>
     </div>
     <script>
-
         //func chang status
         function changStatus(id) {
             // debugger;
@@ -80,7 +88,7 @@
                 $('#check_' + id).css('display', 'none');
             } else {
                 $('#check_' + id).attr('checked', 'checked');
-                $('#check_' + id).css('display', '');
+                $('#check_' + id).css('display', 'none');
             }
             var arrValue = document.getElementById("txtDanhSachChecked").value;
             var array = JSON.parse("[" + arrValue + "]");
@@ -96,19 +104,35 @@
             if (array.length > 0) {
                 $("#btnDisable").css("pointer-events", "auto");
                 $("#btnEnable").css("pointer-events", "auto");
+                $("#btnDisable").css({
+                    "background-color": "#3699FF",
+                    "color": "#fff"
+                });
+                $("#btnEnable").css({
+                    "background-color": "#3699FF",
+                    "color": "#fff"
+                });
             } else {
                 $("#btnDisable").css("pointer-events", "none");
                 $("#btnEnable").css("pointer-events", "none");
+                $("#btnDisable").css({
+                    "background-color": "#F3F6F9",
+                    "color": "#7e8299"
+                });
+                $("#btnEnable").css({
+                    "background-color": "#F3F6F9",
+                    "color": "#7e8299"
+                });
             }
         }
 
-        $(".list_room").change(function () {
+        $(".list_room").change(function() {
             $value = $(this).val();
             $.ajax({
                 method: 'GET',
                 url: '/admin/room-seat/' + $value,
                 dataType: 'html',
-                success: function (data) {
+                success: function(data) {
                     console.log(data)
                     $(".table-responsive").html(data);
                 }
@@ -118,12 +142,12 @@
         //func disable seat
         function disableSeat() {
             Swal.fire({
-                title: '{{__("ConfirmDisableTitle")}}',
+                title: '{{ __('ConfirmDisableTitle') }}',
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonText: '{{__("confirmButtonText")}}',
+                confirmButtonText: '{{ __('confirmButtonText') }}',
                 confirmButtonColor: '#007bff',
-                cancelButtonText: '{{__("cancelButtonText")}}',
+                cancelButtonText: '{{ __('cancelButtonText') }}',
                 cancelButtonColor: '#6c757d',
             }).then((result) => {
                 if (result.value) {
@@ -134,27 +158,27 @@
                         data: {
                             ds_id: ds_id,
                             room: room,
-                            "_token": "{{csrf_token()}}"
+                            "_token": "{{ csrf_token() }}"
                         },
                         method: 'POST',
                         type: 'UPDATE',
                         url: '/admin/room-seat/disable',
                         dataType: 'json',
-                        success: function (data) {
+                        success: function(data) {
                             if (data.success == true) {
                                 Swal.fire({
                                     position: 'center',
-                                    title: '{{__("TextDisableSuccess")}}',
+                                    title: '{{ __('TextDisableSuccess') }}',
                                     icon: "success",
                                     showConfirmButton: false,
                                     timer: 1500
-                                }).then(function () {
+                                }).then(function() {
                                     location.reload();
                                 })
                             } else {
                                 Swal.fire({
                                     position: 'center',
-                                    title: '{{__("TextError")}}',
+                                    title: '{{ __('TextError') }}',
                                     icon: "error",
                                     showConfirmButton: false,
                                     timer: 1500
@@ -169,12 +193,12 @@
         //func enable seat
         function enableSeat() {
             Swal.fire({
-                title: '{{__("ConfirmEnableTitle")}}',
+                title: '{{ __('ConfirmEnableTitle') }}',
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonText: '{{__("confirmButtonText")}}',
+                confirmButtonText: '{{ __('confirmButtonText') }}',
                 confirmButtonColor: '#007bff',
-                cancelButtonText: '{{__("cancelButtonText")}}',
+                cancelButtonText: '{{ __('cancelButtonText') }}',
                 cancelButtonColor: '#6c757d',
             }).then((result) => {
                 if (result.value) {
@@ -184,27 +208,27 @@
                         data: {
                             ds_id: ds_id,
                             room: room,
-                            "_token": "{{csrf_token()}}"
+                            "_token": "{{ csrf_token() }}"
                         },
                         method: 'POST',
                         type: 'UPDATE',
                         url: '/admin/room-seat/enable',
                         dataType: 'json',
-                        success: function (data) {
+                        success: function(data) {
                             if (data.success == true) {
                                 Swal.fire({
                                     position: 'center',
-                                    title: '{{__("TextEnableSuccess")}}',
+                                    title: '{{ __('TextEnableSuccess') }}',
                                     icon: "success",
                                     showConfirmButton: false,
                                     timer: 1500
-                                }).then(function () {
+                                }).then(function() {
                                     location.reload();
                                 })
                             } else {
                                 Swal.fire({
                                     position: 'center',
-                                    title: '{{__("TextError")}}',
+                                    title: '{{ __('TextError') }}',
                                     icon: "error",
                                     showConfirmButton: false,
                                     timer: 1500
